8bb1a7afb9c8269d9d4a2a3f6857f63a6d45dc42
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-09 21:30:01 -0300

    Interation3

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     {allFoods.map((food) => {return(<Lista nome={food.name} imagem={food.image}/>)})}
 
     {/* Interation 2 - the FoodBox component */}
-    <FoodBox foodsObj={`{food.name} {food.calories} {food.image} {food.servings}`} />
+    <FoodBox food={{name: "Orange", calories: 85, image: "https://i.imgur.com/abKGOcv.jpg", servings: 1}} />
 
     {/* Interation 3 - Render a List of FoodBox Components */}
     {allFoods.map((food) => {return(<FoodBox nome={food.name} calorias={food.calories} imagem={food.image} porcoes={food.servings}/>)})}
