387cec1b6ff02981dd3e5f418fb54c6dfefe2223
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-10 08:58:19 -0300

    Interation3

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import './App.css';
 import foodsObj from './foods.json';
 import Lista from './components/lista';
 import FoodBox from './components/foodbox';
-
 import {useState} from "react"; 
+
+// Array to render single element: Orange //
+const food = [ {name: "Single Orange", calories: 85, image: "https://i.imgur.com/abKGOcv.jpg", servings: 1}]
+
 
 function App() {
   
   const [allFoods, setFoods] = useState(foodsObj)
 
-  /* console.log(foods) */
-
-  return <div className="App">
+  return <div className='App'>
 
     {/* // Interation 1 - Render a Simple List */}
-    {allFoods.map((food) => {return(<Lista nome={food.name} imagem={food.image}/>)})}
-
-    {/* Interation 2 - the FoodBox component */}
-    <FoodBox food={{name: "Orange", calories: 85, image: "https://i.imgur.com/abKGOcv.jpg", servings: 1}} />
-
-    {/* Interation 3 - Render a List of FoodBox Components */}
-    {allFoods.map((food) => {return(<FoodBox nome={food.name} calorias={food.calories} imagem={food.image} porcoes={food.servings}/>)})}
+    {foodsObj.map((foodList) => {return(<Lista nome={foodList.name} imagem={foodList.image}/>)})}
   
+    {/* Interation 2 - the FoodBox component */}
+    {food.map((orangeBox) => {return(<FoodBox nome={orangeBox.name} calorias={orangeBox.calories} imagem={orangeBox.image} porcoes={orangeBox.servings}/>)})}
+  
+    {/* Interation 3 - Render a List of FoodBox Components */}
+    <section className='foodbox'>    
+       {allFoods.map((foodAllBox) => {return(<FoodBox nome={foodAllBox.name} calorias={foodAllBox.calories} imagem={foodAllBox.image} porcoes={foodAllBox.servings}/>)})}
+    </section>
+
   </div>
+
 }
 export default App
